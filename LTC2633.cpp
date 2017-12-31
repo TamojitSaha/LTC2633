9664b35c30595c82417e233f6e10f96b59770e48
Sandeepan Sengupta <sengupta.sandeepan@gmail.com>
2017-12-31 20:54:43 +0530

    Refresh #1
    First refresh

--- LTC2633.cpp
+++ LTC2633.cpp
@@ -32,79 +32,89 @@ LTC2633::LTC2633()
 
 }
 
-void LTC2633::begin(uint8_t i2caddr, uint8_t res)
+void LTC2633::begin(uint8_t address, uint8_t resolution, bool frequency)
 {
-  _i2caddr = i2caddr;
-  _res = res;
-  TWCR = 0;
+  addr = address;
+  res = resolution;
+  freq = frequency;
+//  TWCR = 0;
   Wire.begin();
-  Wire.setClock(400000L);
+  if (frequency)
+  {
+    Wire.setClock(400000L);
+  }
+  else
+  {
+    Wire.setClock(100000L);
+  }
   Wire.begin();
 }
 
-void LTC2633::dataman(uint64_t data, uint8_t &data_high, uint8_t &data_low)
+void LTC2633::dataman(uint64_t data)
 {
-  data = constrain(data, 0, pow(2, _res)); /*Constraining data in range relative to resolution*/
+  data = constrain(data, 0, pow(2, res)); /*Constraining data in range relative to resolution*/
   uint16_t Data = (uint16_t)data;
 
-  data = data << (16 - _res);
+  data = data << (16 - res);
+
   data_high = data >> 8;
   data_low  = data & 0xff;
 }
 
 void LTC2633::store(uint16_t data, uint8_t dac)
 {
-  uint8_t data_high, data_low;
-  dataman(data, data_high, data_low);
+  dataman(data);
   writeWire(dac, data_high, data_low); /*writeWire(((write_input_reg << 4) | dac), data_high, data_low)*/
 }
 
 void LTC2633::update(uint8_t dac)
 {
   uint8_t code = (write_input_reg << 4) | dac;
-  writeWire(code, 0x00, 0x00);
+  writeWire(code);
 }
 
 void LTC2633::write(uint16_t data, uint8_t dac)
 {
   uint8_t code = (write_update_dac << 4) | dac;
-
-  uint8_t data_high, data_low;
-  dataman(data, data_high, data_low);
-
+  dataman(data);
   writeWire(code, byte(data_high), byte(data_low));
 }
 
 void LTC2633::internalReference(void)
 {
-  uint8_t code = (internal_reference << 4) | DAC;
-  writeWire(code, 0x00, 0x00);
+  uint8_t code = (internal_reference << 4) | BOTH;
+  control(code);
 }
 
 void LTC2633::externalReference(void)
 {
-  uint8_t code = (external_reference << 4) | DAC;
-  writeWire(code, 0x00, 0x00);
+  uint8_t code = (external_reference << 4) | BOTH;
+  control(code);
 }
 
 void LTC2633::powerDown(uint8_t dac)
 {
   uint8_t code = (power_down_dac << 4) | dac;
-  writeWire(code, 0x00, 0x00);
+  control(code);
 }
 
-void LTC2633::deepSleep(void)
+void LTC2633::powerOff(void)
 {
   uint8_t code = power_down_all;
-  writeWire(code, 0x00, 0x00);
+  control(code);
 }
 
+void LTC2633::control(uint8_t code)
+{
+  Wire.beginTransmission(addr);
+  WIRE_WRITE(code);
+  Wire.endTransmission();
+}
 void LTC2633::writeWire(uint8_t code, uint8_t data_high, uint8_t data_low)
 {
-  Wire.beginTransmission(_i2caddr);
+  Wire.beginTransmission(addr);
   WIRE_WRITE(code);
   WIRE_WRITE(data_high);
   WIRE_WRITE(data_low);
   Wire.endTransmission();
 }
-
