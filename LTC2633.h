bc5fc0b9b2c67c8411392f926476ebe3d60cddbe
Sandeepan Sengupta <sengupta.sandeepan@gmail.com>
2017-12-31 23:30:40 +0530

    Description
    Added detailed description

--- LTC2633.h
+++ LTC2633.h
@@ -2,20 +2,15 @@
     @file     LTC2633.h
     @author   Tamojit Saha(github.com/TamojitSaha),
               Sandeepan Sengupta (sandeepan.info)
-
     Arduino library for Linear Technology LTC®2633
-
     ************************************************
     About LTC®2633
     ************************************************
     The LTC®2633 is a family of dual 12-, 10-, and 8-bit voltage-output DACs with an integrated, high accuracy, low drift reference in an 8-lead TSOT-23 package.
-
     These DACs communicate via a 2-wire I2C-compatible serial interface. The LTC2633 operates in both the standard mode (clock rate of 100kHz) and the fast mode (clock rate of 400kHz).
     *************************************************
-
     v0.1 - Alpha Release
     All text above, must be included in any redistribution
-
 */
 
 #ifndef LTC2633_H
@@ -30,13 +25,13 @@
 #endif
 
 
-//Chip Address Configuration
+/*LTC2633 Address Configuration*/
 #define GND    (0x10)
 #define NC     (0x11)
 #define VCC    (0x12)
 #define GLOBAL (0x73)
 
-//Command Codes
+/*Command Codes*/
 #define write_input_reg     (0x00)  //  Write to Input Register n
 #define update_dac_reg      (0x01)  //  Update (Power-Up) DAC Register n
 #define write_update_all    (0x02)  //  Write to Input Register n, Update (Power-Up) All
@@ -47,7 +42,7 @@
 #define external_reference  (0x07)  //  Select External Reference (Power-Down Internal Reference)
 #define no_operation        (0x0F)  //  No Operation
 
-//Address Codes
+/*Address Codes*/
 #define DAC0                (0x00)  //Select DAC A
 #define DAC1                (0x01)  //Select DAC B
 #define BOTH                (0x0f)  //Select both the DACs
@@ -60,13 +55,13 @@ class LTC2633
     LTC2633();
 
     void begin(uint8_t _address = GLOBAL, uint8_t _resolution = 12, bool _frequency = HIGH);  //Initizlize, uses GLOBAL address and 12bits resolution, by default;
-    void write(uint16_t _data, uint8_t _dac = BOTH);                                          //Write and update ALL DACs, by default
-    void store(uint16_t _data, uint8_t _dac = BOTH);                                          //Stores data to both DAC registers, by default
+    void write(uint64_t _data, uint8_t _dac = BOTH);                                          //Write and update ALL DACs, by default
+    void store(uint64_t _data, uint8_t _dac = BOTH);                                          //Stores data to both DAC registers, by default
     void update(uint8_t _dac = BOTH);                                                         //Updates previously stored value of both DAC registers
     void powerDown(uint8_t _dac = BOTH);                                                      //Power down all DAC, by default
     void powerOff(void);                                                                      //Shutdown entirely
-    void internalReference(void);
-    void externalReference(void);
+    void internalReference(void);                                                             //Switch to internal referance voltage
+    void externalReference(void);                                                             //Switch to external referance voltage
 
   private:
     uint8_t data_high;
@@ -77,8 +72,8 @@ class LTC2633
     bool freq;
     
     void dataman(uint64_t _data);
-    void control(byte _code);
-    void writeWire(byte _code, uint8_t _data_high = 0x00, uint8_t _data_low = 0x00);
+    void control(uint8_t _code);
+    void writeWire(uint8_t _code, uint8_t _data_high = 0x00, uint8_t _data_low = 0x00);
 };
 
 
